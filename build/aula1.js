a0107caee69b492920c154ce3072c465cc88fc28
Bruno Vilela <brunovilela2002@hotmail.com>
2022-09-08 10:37:40 -0300

    primeiro commit

---
+++ build/aula1.js
@@ -0,0 +1,5 @@
+"use strict";
+const nome = "Bruno Vilela Batista";
+console.log("Bem vindo " + nome);
+console.log("aprendendo ts na pratica");
+console.log("aprendendo ts na pratica");
---
+++ src/aula1.ts
@@ -0,0 +1,7 @@
+//comentario para testar remoção feita pelo tsconfig
+
+const nome = "Bruno Vilela Batista";
+
+console.log("Bem vindo "+nome);
+console.log("aprendendo ts na pratica")
+console.log("aprendendo ts na pratica")
